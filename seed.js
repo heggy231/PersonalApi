947ed3dfdcd8adf19978fa185727a079846245d9
Rhea Roy <rroy.rhea@gmail.com>
2019-02-14 16:07:35 -0800

    ran seed again

--- seed.js
+++ seed.js
@@ -12,6 +12,7 @@ db.Movie.create(dummyMovie,(err,newMovie)=>{
 });
 
 
+
 // This file allows us to seed our application with data
 // simply run: `node seed.js` from the root of this project folder.
 
